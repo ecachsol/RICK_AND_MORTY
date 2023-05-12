19899b2f733b7a065db52ee94ff6e102309c20a8
Your Name <you@example.com>
2023-05-12 18:44:06 -0500

    RICK AND MORTY

---
+++ rick_and_morty PROMESAS/Server/src/index.js
@@ -0,0 +1,15 @@
+const http = require('http');
+const {getCharById} = require('./controllers/getCharById')
+const PORT = 3001
+
+http.createServer((req,res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    if(req.url.includes('/rickandmorty/character')){
+        const id = req.url.split('/').at(-1)
+        getCharById(res, id)
+    }
+}).listen(PORT,console.log("port on 3001"))
+
+
+// "/rickandmorty/character/2
